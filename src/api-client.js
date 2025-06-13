851c1549316def0fe8ffc8cbaff7348ba203529c
zzzosia <mrozekzofia@gmail.com>
2025-06-13 22:22:12 +0200

    Create api-client.js

---
+++ src/api-client.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient("https://eohovqvrwlupfctqzhdk.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvaG92cXZyd2x1cGZjdHF6aGRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NzUxMjcsImV4cCI6MjA2MzI1MTEyN30.lvHieYaC0EiorhK-HWaD57AZq_cg7Ui7pKQ1yvuvxQI");
