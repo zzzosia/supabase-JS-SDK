742f0bf8937a73c7b1b96ebcf80372001b985563
zzzosia <mrozekzofia@gmail.com>
2025-06-14 00:39:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,20 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-//import { glob } from 'node:fs/promises'
-import { readdir, stat } from 'node:fs/promises'
 import tailwindcss from '@tailwindcss/vite'
+import fg from 'fast-glob'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-async function findHtmlFiles(dir) {
-  let results = []
-  const entries = await readdir(dir, { withFileTypes: true })
+const repoName = 'supabase-JS-SDK';
 
-  for (const entry of entries) {
-    const fullPath = resolve(dir, entry.name)
-    if (entry.isDirectory()) {
-      const subFiles = await findHtmlFiles(fullPath)
-      results = results.concat(subFiles)
-    } else if (entry.isFile() && fullPath.endsWith('.html')) {
-      results.push(fullPath)
-    }
-  }
-
-  return results
-}
-
-const inputs = await findHtmlFiles(resolve(__dirname, 'src'))
-
-console.log(inputs)
+const inputs = fg.sync('src/**/*.html', { cwd: __dirname, absolute: true })
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-
-  base: '/supabase-JS-SDK/',
+  base: `/${repoName}/`,
+  plugins: [    
+        tailwindcss(),  
+    ],
 
   root: resolve(__dirname, 'src'),
   build: {
